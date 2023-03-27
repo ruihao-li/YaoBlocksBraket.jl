810882e6e0b6f46076ac098c2a45444366b730b2
Ruihao Li <47152529+ruihao-li@users.noreply.github.com>
2023-03-27 15:39:39 -0400

    Lint by JuliaFormatter

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,17 @@ using YaoBlocksBraket
 using Documenter
 
 makedocs(;
-    modules=[YaoBlocksBraket],
-    authors="Ruihao Li",
-    repo="https://github.com/ruihao-li/YaoBlocksBraket.jl/blob/{commit}{path}#{line}",
-    sitename="YaoBlocksBraket.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ruihao-li.github.io/YaoBlocksBraket.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [YaoBlocksBraket],
+    authors = "Ruihao Li",
+    repo = "https://github.com/ruihao-li/YaoBlocksBraket.jl/blob/{commit}{path}#{line}",
+    sitename = "YaoBlocksBraket.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://ruihao-li.github.io/YaoBlocksBraket.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Quickstart" => "index.md", 
-        "API References" => "refs.md"
-    ],
+    pages = ["Quickstart" => "index.md", "API References" => "refs.md"],
 )
 
-deploydocs(;
-    repo="github.com/ruihao-li/YaoBlocksBraket.jl.git",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/ruihao-li/YaoBlocksBraket.jl.git", devbranch = "main")
